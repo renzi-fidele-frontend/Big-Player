ae542bfbf094b1682d7d58ba862e0fecb392dcbe
Renzi Fidele <renzifidele2001@gmail.com>
2023-07-20 22:04:41 +0200

    Avancando

--- src/Pages/Winners/Winners.jsx
+++ src/Pages/Winners/Winners.jsx
@@ -3,7 +3,9 @@ import styles from "./Winners.module.css"
 
 const Winners = () => {
   return (
-    <div id={styles.container}>Winners</div>
+    <div id={styles.container}>
+      
+    </div>
   )
 }
 
