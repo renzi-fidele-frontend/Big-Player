808e88e250827d4d3dab72c9670192d0cb90f8a5
Renzi Fidele <renzifidele2001@gmail.com>
2023-07-21 03:49:01 +0200

    fix

--- src/Pages/Winners/Winners.jsx
+++ src/Pages/Winners/Winners.jsx
@@ -7,19 +7,16 @@ const Winners = () => {
    return (
       <div id={estiloHome.container} className={styles.container}>
          <section id={estiloHome.hero}>
-            
-             
-               <div id={estiloHome.startContainer} className={styles.break}>
-                  <div id={styles.break} className={estiloHome.box1}>
-                     <div className={estiloHome.greyBar}>
-                        <p>Winners</p>
-                     </div>
-                     <GreenPassCard titulo={"Green Pass"} code={"0002"} key={"0002"} data={"01/08/23"} showAllWinners={false} />
-                     <hr style={{ color: "grey", height: "1px", marginBlock: "15px", width: "100%" }} />
-                     <GreenPassCard titulo={"Green Pass"} code={"0002"} key={"0002"} data={"01/08/23"} showAllWinners={false} />
+            <div id={estiloHome.startContainer} className={styles.break}>
+               <div id={styles.break} className={estiloHome.box1}>
+                  <div className={estiloHome.greyBar}>
+                     <p>Winners</p>
                   </div>
+                  <GreenPassCard titulo={"Green Pass"} code={"0002"} key={"0002"} data={"01/08/23"} showAllWinners={false} />
+                  <hr style={{ color: "grey", height: "1px", marginBlock: "15px", width: "100%" }} />
+                  <GreenPassCard titulo={"Green Pass"} code={"0002"} key={"0002"} data={"01/08/23"} showAllWinners={false} />
                </div>
-            
+            </div>
          </section>
       </div>
    );
